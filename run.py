a1fa1e8f31c1a8936a4bb56d6985ac008a27fcfa
Markus <markusbkoch@gmail.com>
2018-10-30 10:34:01 -0300

    import from https://github.com/BlockScience/DiffyQ-SimCAD/commit/16fe59c106bfc8b51025b4256eb305bd238b3a02

--- run.py
+++ run.py
@@ -1,19 +1,24 @@
-from config import state_dict, mechanisms, exogenous_states, env_processes, sim_config
-from configProcessor import generate_config
-from mechanismExecutor import simulation
-from utils import flatten
+from ui.config import state_dict, mechanisms, exogenous_states, env_processes, sim_config
+from engine.configProcessor import generate_config
+from engine.mechanismExecutor import simulation
+from engine.utils import flatten
 
 #from tabulate import tabulate
 import pandas as pd
 
 def main():
     states_list = [state_dict]
-    configs = generate_config(mechanisms, exogenous_states)
+    ep = list(exogenous_states.values())
+    configs = generate_config(mechanisms, ep)
+    # print(configs)
+    # print(states_list)
+    # print(configs)
     # p = pipeline(states_list, configs, env_processes, range(10))
+    T = range(5)
     N = sim_config['N']
-    r = range(5)
+
     # Dimensions: N x r x mechs
-    s = simulation(states_list, configs, env_processes, r, N)
+    s = simulation(states_list, configs, env_processes, T, N)
     result = pd.DataFrame(flatten(s))
     print('Test')
 #    print(tabulate(result, headers='keys', tablefmt='psql'))
