5bfa9d0674640c7ba90f791986740133ad0ef5ee
Markus <markusbkoch@gmail.com>
2018-11-20 14:37:27 -0200

    state values are not reset between runs

--- sandboxUX/config2.py
+++ sandboxUX/config2.py
@@ -7,109 +7,49 @@ from SimCAD.utils.configuration import exo_update_per_ts, proc_trigger, bound_no
 
 
 seed = {
-    'z': np.random.RandomState(1),
-    'a': np.random.RandomState(2),
-    'b': np.random.RandomState(3),
-    'c': np.random.RandomState(3)
+    'a': np.random.RandomState(1),
 }
 
 # Behaviors per Mechanism
-# Different return types per mechanism ?? *** No ***
-def b1m1(step, sL, s):
+def behavior(step, sL, s):
     return {'param1': 1}
-def b2m1(step, sL, s):
-    return {'param2': 4}
-
-def b1m2(step, sL, s):
-    return {'param1': 'a', 'param2': 2}
-def b2m2(step, sL, s):
-    return {'param1': 'b', 'param2': 4}
-
-
-def b1m3(step, sL, s):
-    return {'param1': ['c'], 'param2': np.array([10, 100])}
-def b2m3(step, sL, s):
-    return {'param1': ['d'], 'param2': np.array([20, 200])}
-
 
 # Internal States per Mechanism
-def s1m1(step, sL, s, _input):
+def mechanism(step, sL, s, _input):
     y = 's1'
-    x = _input['param1']
-    return (y, x)
-def s2m1(step, sL, s, _input):
-    y = 's2'
-    x = _input['param2']
-    return (y, x)
-
-def s1m2(step, sL, s, _input):
-    y = 's1'
-    x = _input['param1']
-    return (y, x)
-def s2m2(step, sL, s, _input):
-    y = 's2'
-    x = _input['param2']
-    return (y, x)
-
-def s1m3(step, sL, s, _input):
-    y = 's1'
-    x = _input['param1']
-    return (y, x)
-def s2m3(step, sL, s, _input):
-    y = 's2'
-    x = _input['param2']
+    x = s['s1'] + _input['param1']
     return (y, x)
 
 # Exogenous States
 proc_one_coef_A = 0.7
 proc_one_coef_B = 1.3
-
-def es3p1(step, sL, s, _input):
-    y = 's3'
-    x = s['s3'] * bound_norm_random(seed['a'], proc_one_coef_A, proc_one_coef_B)
+def env_process(step, sL, s, _input):
+    y = 's2'
+    x = bound_norm_random(seed['a'], proc_one_coef_A, proc_one_coef_B)
     return (y, x)
 
-def es4p2(step, sL, s, _input):
-    y = 's4'
-    x = s['s4'] * bound_norm_random(seed['b'], proc_one_coef_A, proc_one_coef_B)
-    return (y, x)
-
-def es5p2(step, sL, s, _input): # accept timedelta instead of timedelta params
+def time(step, sL, s, _input):
     y = 'timestamp'
     x = ep_time_step(s, s['timestamp'], seconds=1)
     return (y, x)
-
-
-# Environment States
-def env_a(x):
-    return 10
-def env_b(x):
-    return 10
-# def what_ever(x):
-#     return x + 1
 
 # Genesis States
 state_dict = {
     's1': Decimal(0.0),
     's2': Decimal(0.0),
-    's3': Decimal(1.0),
-    's4': Decimal(1.0),
     'timestamp': '2018-10-01 15:16:24'
 }
 
 # remove `exo_update_per_ts` to update every ts
 exogenous_states = exo_update_per_ts(
     {
-    "s3": es3p1,
-    "s4": es4p2,
-    "timestamp": es5p2
+    "s2": env_process,
+    "timestamp": time
     }
 )
 
 #	make env proc trigger field agnostic
 env_processes = {
-    "s3": proc_trigger('2018-10-01 15:16:25', env_a),
-    "s4": proc_trigger('2018-10-01 15:16:25', env_b)
 }
 
 # lambdas
@@ -126,41 +66,19 @@ env_processes = {
 # need at least 1 behaviour and 1 state function for the 1st mech with behaviors
 # mechanisms = {}
 mechanisms = {
-    "m1": {
+    "mechanism": {
         "behaviors": {
-            "b1": b1m1, # lambda step, sL, s: s['s1'] + 1,
-            "b2": b2m1
-        },
-        "states": { # exclude only. TypeError: reduce() of empty sequence with no initial value
-            "s1": s1m1,
-            # "s2": s2m1
-        }
-    },
-    "m2": {
-        "behaviors": {
-            "b1": b1m2,
-            "b2": b2m2
+            "behavior": behavior
         },
         "states": {
-            "s1": s1m2,
-            # "s2": s2m2
-        }
-    },
-    "m3": {
-        "behaviors": {
-            "b1": b1m3,
-            "b2": b2m3
-        },
-        "states": {
-            "s1": s1m3,
-            "s2": s2m3
+            "s1": mechanism,
         }
     }
 }
 
 sim_config = {
-    "N": 2,
-    "T": range(5)
+    "N": 3,
+    "T": range(10)
 }
 
 configs.append(Configuration(sim_config, state_dict, seed, exogenous_states, env_processes, mechanisms))
